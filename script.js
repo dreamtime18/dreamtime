af6e5b99c709a70db3d2009aa666ee9d29833c92
student <student@Admins-MacBook-Pro.local>
2019-02-16 15:35:32 -0800

    update

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+  $("#contact").click(hide)
+  function hide (){
+    $("#contact-info").fadeToggle()
+  }
+
+
+  $("#projects").click(hides)
+  function hides (){
+    $("#projects-info").slideToggle()
+  }
+
+  $("#about").click(hidess)
+  function hidess (){
+    $("#about-info").slideToggle()
+  }
+
+
+
+
+
+});
